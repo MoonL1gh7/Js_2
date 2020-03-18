0cb2884d520123d1f95ed3abb3b3d694100cb684
MoonL1gh7 <d1m4tr0sh1n@gmail.com>
2020-03-18 23:21:48 +0300

    Les3HW

--- js/main.js
+++ js/main.js
@@ -1,23 +1,127 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json';
 
-const renderProduct = ({ title = 'Some product', price = 100000, picture = 'https://placehold.it/200x150' }) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <img src="${picture}" alt="no picturre">
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-            </div>`;
-};
+let getRequest = (API, cb) => {
+	fetch(API)
+		.then((result) => result.json())
+		.then((data) => {
+			console.log(data);
+			cb(xhr.responseText);
+		})
+		.catch((error) => {
+			console.log('Error!', error)
+		});
+};		
 
-const renderProducts = (list) => {
-    // Запятые были напечатаны, потому что renderProducts вернул массив.
-    // При объединении всех элементов массива в одну строку запятые были удалены.
-    document.querySelector('.products').innerHTML = list.map((item) => renderProduct(item)).join('');
-};
+// let getRequest = (url, cb) => {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', url, true);
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status !== 200) {
+//         console.log('Error');
+//       } else {
+//         cb(xhr.responseText);
+//       }
+//     }
+//   };
+//   xhr.send();
+// };
 
-renderProducts(products);
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        // this._fetchProducts();
+        this._getProducts()
+            .then((data) => {
+                this.goods = [...data];
+                this.render();
+            });
+    }
+
+  // _fetchProducts() {
+  //   getRequest(`${API}/catalogData.json`, (data) => {
+  //     this.goods = JSON.parse(data);
+  //     this.render();
+  //     console.log(this.goods);
+  //   });
+  // }
+
+    _getProducts() {
+        return fetch(API)
+            .then(result => result.json())
+            .catch(error => {
+                console.log('Error!', error);
+            });
+    }
+
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img='https://placehold.it/200x150') {
+        // this.title = product.title;
+        this.title = product.product_name
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item">
+                    <h3>${this.title}</h3>
+                    <img src="${this.img}" alt="Some img">
+                    <p>${this.price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>`;
+    }
+}
+
+
+class Basket {
+    constructor() {
+        this.products = [];
+    }
+    
+    addProduct(BasketItem) {}
+
+    removeProduct(BasketItem) {}
+
+    clearBasket() {}
+
+    renderTotalSum() {}
+
+    buy() {}
+}
+
+class BasketItem {
+    constructor() {
+        this.productId = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.amount = amount;
+
+    }
+
+    increase(amount) {}
+ 
+    decrease(amount) {}
+
+
+}
+
+new ProductList();
+
